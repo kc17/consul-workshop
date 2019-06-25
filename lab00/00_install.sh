fb174898a7b3f5772a93e438270a14dcb7548c56
Ubuntu <ubuntu@ip-172-31-45-92.us-west-2.compute.internal>
2019-06-25 05:17:17 +0000

    finished lab01, 02

---
+++ lab00/00_install.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+cp ../bin/consul /usr/local/bin
+cp ../bin/consul-template /usr/local/bin
---
+++ lab00/01_test.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+echo "Testing Consul ..."
+consul --version
+
+echo ""
+echo "Testing Consul-Template ..."
+consul-template --version
